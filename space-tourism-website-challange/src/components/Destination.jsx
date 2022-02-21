62a7886ffe7c77854685c8a9df311e6289d3c1bf
William La <william.la59@gmail.com>
2022-02-20 22:16:19 -0500

    destination page functional
    - (bug)reverts back to moon page after refresh. try localstorage for fix
    - need to tweek padding and margins slightly

--- space-tourism-website-challange/src/components/Destination.jsx
+++ space-tourism-website-challange/src/components/Destination.jsx
@@ -1,20 +1,67 @@
 import React from 'react'
+import { useState } from 'react'
+
+import moon from '../assets/destination/image-moon.png'
+import mars from '../assets/destination/image-mars.png'
+import europa from '../assets/destination/image-europa.png'
+import titan from '../assets/destination/image-titan.png'
 
 export const Destination = () => {
+  const [currentDestination, setDestination] = useState(moon);
+  const [currentDestTitle, setDestTitle] = useState("MOON");
+  const [currentDestDesc, setDestDesc] = useState("See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.");
+  const [distance, setDistance] = useState("384,400 KM");
+  const [travelTime, setTravelTime] = useState("3 DAYS");
+
   document.body.className = "destination-page";
   return (
-    <div className='destination-container'>
-      <sub className='sub1'> 0 1 PICK YOUR DESTINATION</sub>
-      <div className="destination-img">
-        <img src="" alt="" />
-      </div>
-      <div className='destination-text'>
-        <div className='destination-nav'>
-
+    <div>
+      
+      <div className='destination-container'>
+        <div className='header-container'>
+        <sub className='sub1 dest-header'> 0 1 PICK YOUR DESTINATION</sub>
         </div>
-        <div className='destination-desc'>
-        <hr></hr>
-        
+        <div className="img-container">
+          <img src={currentDestination} alt="" className="dest-img"/>
+        </div>
+        <div className='destination-text'>
+          <div className='destination-nav'>
+            <ul className="dest-navbar">
+              <li 
+              className='nav-links'
+              onClick={() => {setDestination(moon); setDestTitle("MOON"); setDestDesc("See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites."); setDistance("384,400 KM"); setTravelTime("3 DAYS")}} 
+              style={currentDestination === moon ? {borderBottom: "2px solid white"} :{borderBottom: "2px solid transparent"}}
+              >MOON</li>
+              <li 
+              className='nav-links'
+              onClick={() => {setDestination(mars); setDestTitle("MARS"); setDestDesc("The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin."); setDistance("628 MIL. KM"); setTravelTime("3 YEARS")}} 
+              style={currentDestination === mars ? {borderBottom: "2px solid white"} :{borderBottom: "2px solid transparent"}}
+              >MARS</li>
+              <li 
+               className='nav-links'
+              onClick={() => {setDestination(europa); setDestTitle("EUROPA"); setDestDesc("Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!"); setDistance("225 MIL. KM"); setTravelTime("9 MONTHS")}} 
+              style={currentDestination === europa ? {borderBottom: "2px solid white"} :{borderBottom: "2px solid transparent"}}
+              >EUROPA</li>
+              <li 
+               className='nav-links'
+              onClick={() => {setDestination(titan); setDestTitle("TITAN"); setDestDesc("The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn."); setDistance("1.6 BIL. KM"); setTravelTime("7 YEARS")}} 
+              style={currentDestination === titan ? {borderBottom: "2px solid white"} :{borderBottom: "2px solid transparent"}}
+              >TITAN</li>
+            </ul>
+          </div>
+          <div className='destination-desc'>
+            <h2>{currentDestTitle}</h2>
+            <p>{currentDestDesc}</p>
+            <hr></hr>
+            <div className='distance-text'>
+              <sub className='sub2'>AVG. DISTANCE</sub>
+              <h4>{distance}</h4>
+            </div>
+            <div className='travel-text'>
+              <sub className='sub2'>EST. TRAVEL TIME</sub>
+              <h4>{travelTime}</h4>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- space-tourism-website-challange/src/components/Navbar.jsx
+++ space-tourism-website-challange/src/components/Navbar.jsx
@@ -7,46 +7,46 @@ import closeIcon from "../assets/shared/icon-close.svg"
 import openIcon from "../assets/shared/icon-hamburger.svg"
 
 export const Navbar = () => {
-    
+ 
     const location = useLocation();
     //console.log(location.pathname);
     const [isShowing, setIsShowing] = useState(false);
     const [currentRoute, setCurrentRoute] = useState(location.pathname)
-  return (
-    <header>
-        <div
-        className='navbar-outside'
-        style={isShowing ? {display: "block"} : {display: "none"}}
-        onClick={() => setIsShowing(!isShowing)}
-        ></div>
-        <div className="logo">
-        <img src={logo} alt="" />
-        <hr />
-        </div>
-        <div className="bars-navbar" style={isShowing ? {display: "block"} : {display: "none"}}>
-        <img src={openIcon} alt="" />
-        </div>
-        <ul className="navbar" style={isShowing ? {display: "flex"} : {display: "none"}}>
-            <div className='close' style={isShowing ? {display: "block"} : {display: "none"}}>
-            <img src={closeIcon} alt="" />
+    return (
+        <header>
+            <div
+            className='navbar-outside'
+            style={isShowing ? {display: "block"} : {display: "none"}}
+            onClick={() => setIsShowing(!isShowing)}
+            ></div>
+            <div className="logo">
+            <img src={logo} alt="" />
+            <hr />
             </div>
-            <li>
-                <Link to="/" className="nav-links" style={currentRoute === "/" ? {borderBottom: "2px solid white", backgroundImage: "../assets/home/background-home-desktop.jpg"} :{borderBottom: "2px solid transparent"}} 
-                    onClick={() => setCurrentRoute('/')}><span>00 </span> Home</Link>
-            </li>
-            <li>
-                <Link to="/destination" className="nav-links" style={currentRoute === "/destination" ? {borderBottom: "2px solid white"} :{borderBottom: "2px solid transparent"}} 
-                    onClick={() => setCurrentRoute('/destination')}><span>01</span>  Destination</Link>
-            </li>
-            <li>
-                <Link to="/crew" className="nav-links" style={currentRoute === "/crew" ? {borderBottom: "2px solid white"} :{borderBottom: "2px solid transparent"}} 
-                    onClick={() => setCurrentRoute('/crew')}><span>02 </span> Crew</Link>
-            </li>
-            <li>
-                <Link to="/technology" className="nav-links" style={currentRoute === "/technology" ? {borderBottom: "2px solid white"} :{borderBottom: "2px solid transparent"}} 
-                    onClick={() => setCurrentRoute('/technology')}><span>03 </span> Technology</Link>
-            </li>
-            </ul>
-    </header>
-  )
+            <div className="bars-navbar" style={isShowing ? {display: "block"} : {display: "none"}}>
+            <img src={openIcon} alt="" />
+            </div>
+            <ul className="navbar" style={isShowing ? {display: "flex"} : {display: "none"}}>
+                <div className='close' style={isShowing ? {display: "block"} : {display: "none"}}>
+                <img src={closeIcon} alt="" />
+                </div>
+                <li>
+                    <Link to="/" className="nav-links" style={currentRoute === "/" ? {borderBottom: "2px solid white"} :{borderBottom: "2px solid transparent"}} 
+                        onClick={() => setCurrentRoute('/')}><span>00 </span> Home</Link>
+                </li>
+                <li>
+                    <Link to="/destination" className="nav-links" style={currentRoute === "/destination" ? {borderBottom: "2px solid white"} :{borderBottom: "2px solid transparent"}} 
+                        onClick={() => setCurrentRoute('/destination')}><span>01</span>  Destination</Link>
+                </li>
+                <li>
+                    <Link to="/crew" className="nav-links" style={currentRoute === "/crew" ? {borderBottom: "2px solid white"} :{borderBottom: "2px solid transparent"}} 
+                        onClick={() => setCurrentRoute('/crew')}><span>02 </span> Crew</Link>
+                </li>
+                <li>
+                    <Link to="/technology" className="nav-links" style={currentRoute === "/technology" ? {borderBottom: "2px solid white"} :{borderBottom: "2px solid transparent"}} 
+                        onClick={() => setCurrentRoute('/technology')}><span>03 </span> Technology</Link>
+                </li>
+                </ul>
+        </header>
+    )
 }
