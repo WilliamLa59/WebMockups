d50258961a16c061e2306ec4ae3f53affefa52b5
William La <william.la59@gmail.com>
2022-03-03 19:34:16 -0500

    tech page almost done
    - need to have pictures change to mobile versions
    
    -issue with destination scaling on screen size 320 and smaller
    
    -changed burger icon size as well as close button alignment to match

--- space-tourism-website-challange/src/components/Technology.jsx
+++ space-tourism-website-challange/src/components/Technology.jsx
@@ -5,7 +5,7 @@ import portImg from "../assets/technology/image-spaceport-portrait.jpg";
 import capsuleImg from "../assets/technology/image-space-capsule-portrait.jpg";
 
 export const Technology = () => {
-  const Vehicle = {name: "SPACE VEHICLE", desc: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our  WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"};
+  const Vehicle = {name: "LAUNCH VEHICLE", desc: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our  WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"};
   const Port = {name: "SPACEPORT", desc: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."};
   const Capsule = {name: "SPACE CAPSULE", desc: " A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."};
 
@@ -30,7 +30,7 @@ export const Technology = () => {
 
         <div className='tech-nav-btn' 
         onClick={()=>{setCurrentTech(Vehicle); setCurrentTechImg(vehicleImg);}}
-        style={currentTech.name === "SPACE VEHICLE" ? {backgroundColor: '#fff' , color: '#0B0D17'} : {backgroundColor: 'transparent'}}
+        style={currentTech.name === "LAUNCH VEHICLE" ? {backgroundColor: '#fff' , color: '#0B0D17'} : {backgroundColor: 'transparent'}}
         >1</div>
 
         <div className='tech-nav-btn middle-btn'
