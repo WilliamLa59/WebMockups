2a252e68d71cb3e20f9611de7b436925bb460617
William La <william.la59@gmail.com>
2022-03-10 15:12:21 -0500

    lead layout done
    - just now found out the design provided is for 1440x810p
    
    -readjusted some styling to better match that resolution.

--- blogr-landing-page/src/components/Hero/Hero.jsx
+++ blogr-landing-page/src/components/Hero/Hero.jsx
@@ -13,7 +13,7 @@ export const Hero = () => {
 
         <Center>
           <div className='hero-header'>
-            <Text fontSize="100" fontWeight="500" letterSpacing="0.5">
+            <Text fontSize="68" fontWeight="500" letterSpacing="0.5">
               A mondern publishing platform
             </Text>
           </div>
@@ -21,7 +21,7 @@ export const Hero = () => {
 
         <Center>
           <div className='hero-sub'>
-            <Text fontSize="28" fontWeight="500">
+            <Text fontSize="18" fontWeight="500">
               Grow your audience and build your online brand
             </Text>
           </div>
--- blogr-landing-page/src/components/Lead/Lead.jsx
+++ blogr-landing-page/src/components/Lead/Lead.jsx
@@ -1,39 +1,49 @@
 import React from 'react'
-import { Text } from '@chakra-ui/react'
+import { Text, Center } from '@chakra-ui/react'
+import editor_desktop from '../../StartingFiles/images/illustration-editor-desktop.svg';
+import './Lead.scss'
+
 export const Lead = () => {
   return (
     <div className='lead-container'>
-      <div className='lead-header'>
-        <Text>
-          Designed for the future
-        </Text>
-      </div>
-
-      <div className='lead-text-container'>
-        <div>
-          <Text>Introducing and extensible editor</Text>
-          <Text>
-            Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. 
-            The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, 
-            videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or 
-            change the looks of a blog.
+      <Center>
+        <div className='lead-header'>
+          <Text fontSize='36' fontWeight="bold">
+            Designed for the future
           </Text>
         </div>
+      </Center>
 
-        <div>
-          <Text>Robust content management</Text>
-          <Text>
-          Flexible content management enables users to easily move through posts. Increase the usability of your blog 
-          by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.
+      <div className='grid-container'>
+        <Center>
+          <div className='lead-text-container'>
 
-          </Text>
+            <div>
+              <Text fontSize='20' fontWeight="bold" className='text-title'>Introducing and extensible editor</Text>
+              <Text fontSize='16'>
+                Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. 
+                The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, 
+                videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or 
+                change the looks of a blog.
+              </Text>
+            </div>
+
+            <div>
+              <Text fontSize='20' fontWeight="bold" className='text-title text-spacer'>Robust content management</Text>
+              <Text fontSize='16'>
+                Flexible content management enables users to easily move through posts. Increase the usability of your blog 
+                by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.
+              </Text>
+            </div>
+          </div>
+        </Center>
+      
+        <div className='lead-img-container'>
+          <img src={editor_desktop} alt="" />
         </div>
-      </div>
-    
-      <div className='lead-img-container'>
 
       </div>
-
+      
     </div>
   )
 }
